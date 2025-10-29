02977963a9cbb96a451ff44ed98293f2f3e8683d
taigazeal <zeal24460149@fsgcl.jp>
2025-10-29 16:11:54 +0900

    地図

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,12 @@ android {
 }
 
 dependencies {
+  //implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
