abbcaaba06a59f176c457362d63a0eb089334cfb
taigazeal <zeal24460149@fsgcl.jp>
2025-10-31 11:15:07 +0900

    マップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-  //implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
