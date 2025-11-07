12463b59aa1f39b5040984c7021dac91e30d6ca8
H-KIKINA <zeal24468216@fsgcl.jp>
2025-11-07 12:11:02 +0900

    SOS通話

--- app/src/main/java/com/example/sosbaton/SosActivity.java
+++ app/src/main/java/com/example/sosbaton/SosActivity.java
@@ -2,6 +2,12 @@ package com.example.sosbaton;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+
+
 
 public class SosActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class SosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // activity_sos.xml を画面として使う
         setContentView(R.layout.activity_sos);
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> finish()); // ← 戻る処理
+
+        Button HelpButton = findViewById(R.id.HelpButton);
+        HelpButton.setOnClickListener(v -> {
+            // 電話アプリを起動して119を入力した状態にする
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:119"));
+            startActivity(intent);
+        });
+
+        Button EmergencyButton = findViewById(R.id.EmergencyButton);
+        EmergencyButton.setOnClickListener(v -> {
+            // 電話アプリを起動して119を入力した状態にする
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:110"));
+            startActivity(intent);
+        });
     }
+
+
 }
